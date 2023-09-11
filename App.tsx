00d44a4db3f60f7074f20128e5c7fc3476bf546e
David <davidpanesiu@gmail.com>
2023-09-11 13:32:32 -0400

    added and configured amplify, added peer dependencies for web build

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import 'react-native-gesture-handler';
 import useColorScheme from './app/hooks/useColorScheme';
 import Navigation from './app/navigation';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+Amplify.configure(awsExports);
+
 export default function App() {
 
   const colorScheme = useColorScheme();
