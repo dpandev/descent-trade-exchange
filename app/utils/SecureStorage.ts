a3ddcdbfd342d0edb5274b08fab3c99809ae847a
David Panesiu <davidpanesiu@gmail.com>
2023-10-30 18:20:54 -0400

    removed expo-secure-store

--- app/utils/SecureStorage.ts
+++
@@ -1,9 +0,0 @@
-import * as SecureStore from 'expo-secure-store';
-
-export async function save(key: string, value: string) {
-  await SecureStore.setItemAsync(key, value);
-}
-
-export async function getValueFor(key: string) {
-  await SecureStore.getItemAsync(key);
-}
